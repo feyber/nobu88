ef100c705644951d6e82ce2ae1ba670689006e32
onlinesuksesabadi <feyberho@gmail.com>
2021-11-09 20:37:20 +0700

    upload aja

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,235 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <body className="bg-bgnobu select-none">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>
+          NOBU88 Let's Play Play Bosku | Situs Judi Online Terpercaya | Visi
+          Depo Kecil Misi WD Besar — Nobu88
+        </title>
+        <link rel="shortcut icon" type="image/jpg" href="/favicon" />
+        <meta
+          name="description"
+          content="Nobu88 adalah Agen slot online terpercaya yang menyediakan banyak permainan Slot dengan Deposit pulsa dan Ewallet. Situs judi online paling lengkap dan terpopuler."
+        />
+        <meta
+          name="keywords"
+          content="Nobu88, Agen Slot, Situs Judi, Situs slot, Agen Slot pulsa, Slot pulsa, permainan slot, Slot online terbaik. Slot Online"
+        />
       </Head>
+      <div className="max-w-5xl mx-auto flex flex-col items-start justify-items-start p-4">
+        <header className="py-8 px-0 md:px-4">
+          <nav className="flex lg:justify-start lg:items-start justify-center items-center">
+            <Link href="/" passHref>
+              <a className="cursor-pointer">
+                <Image
+                  src="/nobu88.png"
+                  alt="Logo Nobu88"
+                  width="180"
+                  height="50"
+                />
+              </a>
+            </Link>
+          </nav>
+        </header>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+        {/* hero */}
+        <div className="mx-auto flex flex-col justify-center items-center">
+          <div className="w-full text-center bg-gradient-to-r from-gold via-litegold to-golden py-4 px-4 rounded-lg">
+            <h1 className="uppercase text-xs lg:text-3xl font-bold text-black hover:text-gray-900 w-full md:scale-105 md:hover:scale-95 duration-200">
+              NOBU88 Situs Judi Slot Online Terlengkap & Terpercaya
+            </h1>
+          </div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className="flex flex-col md:flex-row justify-between mx-auto items-center mt-8 mb-12">
+            <div>
+              <div
+                className="grid md:grid-cols-3 md:grid-rows-2 gap-2 md:gap-4 items-center grid-cols-2
+                grid-rows-3"
+              >
+                <a
+                  href="https://188.166.211.237/slot/pragmatic_slot"
+                  className=" bg-gradient-to-r from-gold via-litegold to-golden py-4 px-8 rounded-lg hover:scale-105 duration-500"
+                >
+                  <Image
+                    src="/jackpot.png"
+                    alt="slot"
+                    width="200"
+                    height="200"
+                  />
+                  <p className="text-black font-bold text-center">SLOT</p>
+                </a>
+                <a
+                  href="https://188.166.211.237/casino"
+                  className="bg-gradient-to-r from-gold via-litegold to-golden py-4 px-8 rounded-lg hover:scale-105 duration-500"
+                >
+                  <Image
+                    src="/casino.png"
+                    alt="casino"
+                    width="200"
+                    height="200"
+                  />
+                  <p className="text-black font-bold text-center">CASINO</p>
+                </a>
+                <a
+                  href="https://188.166.211.237/games/pragmatic_game"
+                  className="bg-gradient-to-r from-gold via-litegold to-golden py-4 px-8 rounded-lg hover:scale-105 duration-500"
+                >
+                  <Image
+                    src="/games.png"
+                    alt="games"
+                    width="200"
+                    height="200"
+                  />
+                  <p className="text-black font-bold text-center">GAMES</p>
+                </a>
+                <a
+                  href="https://188.166.211.237/sports/ibc_sports_sports"
+                  className="bg-gradient-to-r from-gold via-litegold to-golden py-4 px-8 rounded-lg hover:scale-105 duration-500"
+                >
+                  <Image
+                    src="/sport.png"
+                    alt="sport"
+                    width="200"
+                    height="200"
+                  />
+                  <p className="text-black font-bold text-center">SPORT</p>
+                </a>
+                <a
+                  href="https://188.166.211.237/promo"
+                  className="bg-gradient-to-r from-gold via-litegold to-golden py-4 px-8 rounded-lg hover:scale-105 duration-500"
+                >
+                  <Image
+                    src="/bonus.png"
+                    alt="bonus"
+                    width="200"
+                    height="200"
+                  />
+                  <p className="text-black font-bold text-center">BONUS</p>
+                </a>
+                <div className="bg-gradient-to-r from-gold via-litegold to-golden py-4 px-8 rounded-lg hover:scale-105 duration-500">
+                  <Image src="/win.png" alt="menang" width="200" height="200" />
+                  <p className="text-black font-bold text-center">99.9%</p>
+                </div>
+              </div>
+            </div>
+            <div className="hidden lg:block scale-110 hover:scale-105 duration-500">
+              <Image
+                src="/slot.gif"
+                alt="tunjukin tempat slot"
+                width="600"
+                height="500"
+              />
+            </div>
+          </div>
+          {/* login & register */}
+          <div className="container flex justify-center items-center text-center gap-2">
+            <a
+              href="https://188.166.211.237/invite/zaz8dmz"
+              className=" flex flex-col md:flex-row animate-pulse ring-2 ring-red-700 duration-500 ring-opacity-50 focus:ring-offset-2 hover:ring-opacity-100  items-center justify-center w-1/4 bg-gradient-to-r from-gold via-litegold to-golden text-2xl rounded-lg h-24"
+            >
+              <span className="mr-1">
+                {" "}
+                <Image src="/login.png" alt="login" width="50" height="50" />
+              </span>{" "}
+              <p className="text-xs md:text-base lg:text-lg">Login</p>
+            </a>
+            <a
+              href="https://188.166.211.237/invite/zaz8dmz"
+              className=" flex flex-col md:flex-row animate-pulse items-center ring-2 duration-500 ring-red-700 ring-opacity-50 focus:ring-offset-2 hover:ring-opacity-100 justify-center w-1/4 bg-gradient-to-r from-gold via-litegold to-golden text-2xl rounded-lg h-24"
+            >
+              <span className="mr-1">
+                {" "}
+                <Image src="/register.png" alt="login" width="50" height="50" />
+              </span>{" "}
+              <p className="text-xs md:text-base lg:text-lg">Daftar</p>
+            </a>
+            <a
+              href="https://direct.lc.chat/13023216/"
+              className=" flex flex-col md:flex-row items-center justify-center w-1/4 bg-gradient-to-r from-gold via-litegold to-golden text-2xl rounded-lg h-24"
+            >
+              <span className="mr-1">
+                {" "}
+                <Image src="/cs.png" alt="login" width="50" height="50" />
+              </span>{" "}
+              <p className="text-xs md:text-base lg:text-lg">Kontak CS</p>
+            </a>
+            <a
+              href="https://wa.me/6282122014931"
+              className=" flex flex-col md:flex-row items-center justify-center w-1/4 bg-gradient-to-r from-gold via-litegold to-golden text-2xl rounded-lg h-24"
+            >
+              <span className="mr-1">
+                {" "}
+                <Image src="/wa.png" alt="login" width="50" height="50" />
+              </span>{" "}
+              <p className="text-xs md:text-base lg:text-lg">Whatsapp</p>
+            </a>
+          </div>
         </div>
-      </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
+        <div className="p-4 md:p-8 lg:p-4 mx-auto flex flex-col justify-center items-center mt-10">
+          <div className="relative group ">
+            <div className="absolute md:-inset-1 -inset-0 bg-gradient-to-r from-gold via-litegold to-golden rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-700 group-hover:duration-200 animate-tilt"></div>
+            <div className="relative px-7 py-4 bg-black rounded-lg leading-none flex items-center">
+              <span className="flex flex-col items-center text-justify uppercase">
+                <span className="mt-4 mb-2 text-center text-sm md:text-lg lg:text-xl text-yellow-50 group-hover:text-yellow-100 transition duration-200">
+                  NOBU88 Situs Judi Slot Online Terbaik dan Mudah Jackpot
+                </span>
+                <span className="mt-4 text-yellow-50 group-hover:text-yellow-100 transition duration-200 text-xs md:text-sm lg:text-base">
+                  <a
+                    href="https://188.166.211.237/invite/zaz8dmz"
+                    className="text-yellow-500"
+                  >
+                    NOBU88
+                  </a>{" "}
+                  Situs judi Slot Online terpercaya uang asli Rupiah solusi judi
+                  online terpopuler saat ini slot online memberikan joker123
+                  judi slot modern saat ini. Judi Slot online saat ini sangat
+                  populer mudah dimainkan dengan modal kecil bisa mendapatkan
+                  keuntungan berlipat-lipat dari Free-spin dan jackpot yang
+                  didapatkan saat bermain.sejarah lahir dicetuskan oleh bandar
+                  slot online yang merubah sistem taruhan slot menjadi online
+                  yang bisa dimainkan dengan ponsel dan pc dimana aja anda
+                  berada mempermudah tidak harus pergi kemanapun udah bisa
+                  melakukan deposit dan withdraw.
+                </span>
+                <span class="mt-4 text-yellow-50 group-hover:text-yellow-100 transition duration-200 text-xs md:text-sm lg:text-base">
+                  Slot Online terpercaya NOBU88 sangat membuat banyak player
+                  tidak mau berpindah ke situs slot online dimana NOBU88
+                  memberikan bonus new member dan Rollingan banyak besar membuat
+                  player nyaman bermain.
+                </span>
+                <span class="mt-4 text-yellow-50 group-hover:text-yellow-100 transition duration-200 text-xs md:text-sm lg:text-base">
+                  Situs Slot terpercaya NOBU88 memberikan jackpot slot online
+                  ratusan juta setiap hari membuat banyak player menanti untuk
+                  mendapatkan kemenangan tersebut slot online Populer game
+                  disaat ini yang bisa dimainkan oleh tua dan muda mudah
+                  dimengerti dimana tujuan utama dimana harus memiliki gambar
+                  yang sama untuk mendapatkan hadiah yang berlipat-lipat
+                  dinamakan Jackpot.
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Ini footer */}
+      <footer className="mx-auto mt-10 mb-2 flex justify-center items-center text-yellow-50 text-xs md:text-base lg:text-lg">
+        <div>
+          Copyright &copy; 2021{" "}
+          <a
+            href="https://188.166.211.237/invite/zaz8dmz"
+            className="text-yellow-100"
+          >
+            NOBU88
+          </a>{" "}
+          | All Right Reserved 18+
+        </div>
       </footer>
-    </div>
-  )
+    </body>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,40 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: "#ffdf00",
+        golden: "#FFD700",
+        brgold: "#FFE55C",
+        litegold: "#FFED8A",
+        bgnobu: "#262626",
+      },
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+      animation: {
+        tilt: "tilt 10s infinite linear",
+      },
+      keyframes: {
+        tilt: {
+          "0%, 50%, 100%": {
+            transform: "rotate(0deg)",
+          },
+          "25%": {
+            transform: "rotate(0.5deg)",
+          },
+          "75%": {
+            transform: "rotate(-0.5deg)",
+          },
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
